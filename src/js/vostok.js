80302f1de3b132b6815f6745576d71e07fbf0f48
Ekaterina Volkova <ekatevolkova@gmail.com>
2020-10-17 18:57:10 +0200

    animation and scroll draft - done

--- src/js/vostok.js
+++ src/js/vostok.js
@@ -1,33 +1,69 @@
-const lines = document.querySelectorAll('div.line1, div.line2')
+const allLines = document.querySelectorAll('div.line1, div.line3, div.line2, div.line4, div.line5, div.line6')
+const lines1 = document.querySelectorAll('div.line1, div.line3, div.line5')
+const lines2 = document.querySelectorAll('div.line2, div.line4, div.line6')
+
 const line1 = document.querySelector('div.line1')
 const line2 = document.querySelector('div.line2')
+
 const letters = document.querySelector('div.letters')
 
-const spanTag1 = line1.querySelector('span')
-const spanTag2 = line2.querySelector('span')
-const spanWidth1 = spanTag1.clientWidth
-const spanWidth2 = spanTag2.clientWidth
+const spanTag = line1.querySelector('span')
+const spanWidth = spanTag.clientWidth
 
-function runningLetters () {
-    for (let i = 0; i < 20; i = i + 1) {
-        line1.appendChild(spanTag1.cloneNode(true))
-        line2.appendChild(spanTag2.cloneNode(true))
+const cursorClip = document.querySelector('div.collage')
+
+const mq = window.matchMedia("(min-width: 600px) and (prefers-reduced-motion: no-preference)")
+
+const runScripts = function () {
+    if (mq.matches) {
+
+    allLines.forEach(line => {
+        const span = line.querySelector('span')
+        for (let i = 0; i < 10; i = i + 1) {
+            line.appendChild(span.cloneNode(true))
+        }
+    })
+
+    const lines1Timeline = gsap.timeline({
+        repeat: -1
+    })
+    const lines2Timeline = gsap.timeline({
+        repeat: -1
+    })
+    
+    lines1Timeline
+        .set(lines1, {x: 0})
+        .to(lines1, {x: spanWidth * - 1, duration: 60, ease: 'linear'})
+    
+    lines2Timeline
+        .set(lines2, {x: 0 })
+        .to(lines2, {x: spanWidth , duration: 60, ease: 'linear'})    
     }
-    
-    const line1Timeline = gsap.timeline({
-        repeat: -1
-    })
-    const line2Timeline = gsap.timeline({
-        repeat: -1
-    })
-    
-    line1Timeline
-        .set(line1, {x: 0})
-        .to(line1, {x: spanWidth1 * - 1, duration: 30, ease: 'linear'})
-    
-    line2Timeline
-        .set(line2, {x: 0 })
-        .to(line2, {x: spanWidth2 , duration: 30, ease: 'linear'})    
-}
 
-runningLetters()
+
+
+
+
+
+        let cursorCurrentX = 0
+        let cursorCurrentY = 0
+        let cursorAimX = 0
+        let cursorAimY = 0
+
+    const changeCursor = function () {
+        cursorCurrentX = cursorCurrentX + (cursorAimX - cursorCurrentX) * 0.15
+        cursorCurrentY = cursorCurrentY + (cursorAimY - cursorCurrentY) * 0.15
+
+        cursorClip.style.clipPath = `circle(8% at ${cursorCurrentX}px ${cursorCurrentY}px)`
+
+        requestAnimationFrame(changeCursor)
+    }
+    document.addEventListener('mousemove', function (event) {
+        cursorAimX = event.pageX 
+        cursorAimY = event.pageY 
+    }) 
+
+    changeCursor()
+
+}
+runScripts()

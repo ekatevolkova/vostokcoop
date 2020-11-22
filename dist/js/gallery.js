4a88922526a520a147efa407408efd3744ae53ef
Ekaterina Volkova <ekatevolkova@gmail.com>
2020-11-22 14:21:45 +0100

    fixing

--- dist/js/gallery.js
+++ dist/js/gallery.js
@@ -5,113 +5,113 @@ let container = document.getElementById("container");
 
 ScrollTrigger.matchMedia({
 
-"(min-width: 900px)": function() {
+  "(min-width: 901px)": function () {
 
-gsap.to(container, {
-  x: () => -(container.scrollWidth - document.documentElement.clientWidth) + "px",
-  ease: "none",
-  scrollTrigger: {
-    trigger: container,
-    invalidateOnRefresh: true,
-    pin: true,
-    scrub: 1,
-    end: () => "+=" + container.offsetWidth
+    gsap.to(container, {
+      x: () => -(container.scrollWidth - document.documentElement.clientWidth) + "px",
+      ease: "none",
+      scrollTrigger: {
+        trigger: container,
+        invalidateOnRefresh: true,
+        pin: true,
+        scrub: 1,
+        end: () => "+=" + container.offsetWidth
+      }
+    })
+
   }
-})
-
-}
 })
 
 ScrollTrigger.matchMedia({
 
-  "(max-width: 900px)": function() {
-  
-  gsap.to(".square", {
-    scrollTrigger: {
-      trigger: ".square",
-      start: "top 90%",
-      scrub: 0.5
-    },
-    x: "25%",
-    ease: "none"
-    
-  })
-  
-  gsap.to(".monument", {
-    scrollTrigger: {
-      trigger: ".monument",
-      start: "top 90%",
-      scrub: 0.5
-    },
-    x: "-100vw",
-    ease: "none"
-  })
+  "(max-width: 900px)": function () {
+
+    gsap.to(".square", {
+      scrollTrigger: {
+        trigger: ".square",
+        start: "top 90%",
+        scrub: 0.5
+      },
+      x: "25%",
+      ease: "none"
+
+    })
+
+    gsap.to(".monument", {
+      scrollTrigger: {
+        trigger: ".monument",
+        start: "top 90%",
+        scrub: 0.5
+      },
+      x: "-100vw",
+      ease: "none"
+    })
 
 
 
-  gsap.to(".nepal", {
-    scrollTrigger: {
-      trigger: ".nepal",
-      start: "top 90%",
-      scrub: 0.5
-    },
-    x: "15%",
-    ease: "none"
-  })
+    gsap.to(".nepal", {
+      scrollTrigger: {
+        trigger: ".nepal",
+        start: "top 90%",
+        scrub: 0.5
+      },
+      x: "15%",
+      ease: "none"
+    })
 
-  gsap.to(".scheme", {
-    scrollTrigger: {
-      trigger: ".scheme",
-      start: "top 30%",
-      // scrub: 0.5
-    },
-    x: "-200%",
-    y: "-50%",
-    ease: "none"
-  })
+    gsap.to(".scheme", {
+      scrollTrigger: {
+        trigger: ".scheme",
+        start: "top 30%",
+        scrub: 0.5
+      },
+      x: "-200%",
+      y: "-50%",
+      ease: "none"
+    })
 
-  gsap.to(".sound", {
-    scrollTrigger: {
-      trigger: ".sound",
-      start: "top 80%",
-      scrub: 0.5
-    },
-    x: "-50%",
-    ease: "none"
-  })
+    gsap.to(".sound", {
+      scrollTrigger: {
+        trigger: ".sound",
+        start: "top 80%",
+        scrub: 0.5
+      },
+      x: "-50%",
+      ease: "none"
+    })
 
-  gsap.to(".stand", {
-    scrollTrigger: {
-      trigger: ".stand",
-      start: "top 80%",
-      scrub: 0.5
-    },
-    ease: "none",
-    scale: 1.5
-  })
+    gsap.to(".stand", {
+      scrollTrigger: {
+        trigger: ".stand",
+        start: "top 80%",
+        scrub: 0.5
+      },
+      ease: "none",
+      scale: 1.5
+    })
 
 
   }
-  })
+})
 
 
   ScrollTrigger.matchMedia({
 
-    "(max-width: 600px)": function() {
-    
-  gsap.to(".scheme", {
-    scrollTrigger: {
-      trigger: ".scheme",
-      start: "top 30%",
-      // scrub: 0.5
-    },
-    x: "-150%",
-    y: "-50%",
-    ease: "none"
-  })
+    "(max-width: 600px)": function () {
 
-}
-})
+      gsap.to(".scheme", {
+        scrollTrigger: {
+          trigger: ".scheme",
+          start: "top 30%",
+          scrub: 0.5
+        },
+        x: "-150%",
+        y: "-50%",
+        ease: "none"
+      })
+
+    }
+  })
 // gsap.utils.toArray('.frame').forEach(frame=> {
 //   ScrollTrigger.create({
 //       trigger: frame,
@@ -121,4 +121,3 @@ ScrollTrigger.matchMedia({
 //       toggleClass: 'in-view'
 //   });
 // });
-          
